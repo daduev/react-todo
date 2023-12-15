69e9e3e12b06160133892c0518f40ac3943bc358
daduev <daduev.ad@gmail.com>
2023-12-15 12:07:16 +0200

    Updated service api

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -8,16 +8,16 @@ export const TodoProvider = ({ children }) => {
   const [todos, setTodos] = useState([]);
   const [isLoading, setLoading] = useState(false);
 
+  /*
   useEffect(() => {
     get();
   }, [isLoading, user.username]);
+*/
 
   const get = async () => {
-    const response = await fetch(
-      "https://my-local-dev/api/private/todo", {
-        credentials: "include",
-      }
-    );
+    const response = await fetch("https://my-local-dev/api/private/todo", {
+      credentials: "include",
+    });
 
     if (response.status === 200) {
       const data = await response.json();
@@ -30,12 +30,12 @@ export const TodoProvider = ({ children }) => {
   const add = async () => {
     setLoading(true);
     let response = await fetch("https://my-local-dev/api/private/todo/add", {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify({
-        text: null
+        text: null,
       }),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
       credentials: "include",
     });
@@ -44,18 +44,21 @@ export const TodoProvider = ({ children }) => {
 
   const update = async (id) => {
     setLoading(true);
-    let newText = todos.filter(el => el.id === id)[0].text;
+    let newText = todos.filter((el) => el.id === id)[0].text;
 
-    let response = await fetch(`https://my-local-dev/api/private/todo/${id}/edit`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        text: newText,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-      credentials: "include",
-    });
+    let response = await fetch(
+      `https://my-local-dev/api/private/todo/${id}/edit`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          text: newText,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        credentials: "include",
+      }
+    );
     setLoading(false);
   };
 
@@ -64,7 +67,7 @@ export const TodoProvider = ({ children }) => {
     let response = await fetch(
       `https://my-local-dev/api/private/todo/${id}/delete`,
       {
-        method: 'DELETE',
+        method: "DELETE",
         credentials: "include",
       }
     );
@@ -82,20 +85,23 @@ export const TodoProvider = ({ children }) => {
 
   const complete = async (id) => {
     setLoading(true);
-    let done = !todos.filter(el => el.id === id)[0].done;
+    let done = !todos.filter((el) => el.id === id)[0].done;
 
-    let response = await fetch(`https://my-local-dev/api/private/todo/${id}/done/${done}`, {
-      method: 'PUT',
-      body: JSON.stringify({
-        done
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-      credentials: "include",
-    });
+    let response = await fetch(
+      `https://my-local-dev/api/private/todo/${id}/done/${done}`,
+      {
+        method: "PUT",
+        body: JSON.stringify({
+          done,
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        credentials: "include",
+      }
+    );
     setLoading(false);
-  }
+  };
 
   return (
     <TodoContext.Provider
@@ -105,7 +111,7 @@ export const TodoProvider = ({ children }) => {
         add,
         update,
         del,
-        complete
+        complete,
       }}
     >
       {children}
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -3,67 +3,67 @@ import { createContext, useContext, useState, useEffect } from "react";
 const UserContext = createContext(undefined);
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState({});
+  const [user, setUser] = useState({});
 
-    useEffect(() => {
-        const getCurrentUser = async () => {
-            const response = await fetch(
-                "https://my-local-dev/api/private/currentUser",
-                {
-                    credentials: "include",
-                }
-            );
-            if (response.status === 200) {
-                const data = await response.json();
-                if (data.username) {
-                    setUser({ username: data.username });
-                }
-            }
-        };
-        getCurrentUser();
-    }, [user.username]);
+  const domain = "https://dev-walx.onrender.com";
+  //const domain = "http://localhost:8080";
 
-    const login = async (formData) => {
-        let response = await fetch("https://my-local-dev/api/public/login", {
-            method: 'POST',
-            credentials: "include",
-            body: JSON.stringify({
-                username: formData.username,
-                password: formData.password
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        });
-
-        if (response.status === 200) {
-            let data = await response.json();
-            setUser({ username: data.username });
+  useEffect(() => {
+    const getCurrentUser = async () => {
+      const response = await fetch(`${domain}/api/private/users/current`, {
+        credentials: "include",
+      });
+      if (response.status === 200) {
+        const data = await response.json();
+        if (data.username) {
+          setUser({ username: data.username });
         }
+      }
     };
+    getCurrentUser();
+  }, [user.username]);
 
-    const logout = async() => {
-        let response = await fetch("https://my-local-dev/api/public/logout", {
-            method: 'POST',
-            credentials: "include",
-        });
+  const login = async (formData) => {
+    let response = await fetch(`${domain}/api/public/login`, {
+      method: "POST",
+      credentials: "include",
+      body: JSON.stringify({
+        username: formData.username,
+        password: formData.password,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
 
-        if (response.status === 200) {
-            setUser({});
-        }
+    if (response.status === 200) {
+      let data = await response.json();
+      setUser({ username: data.username });
     }
+  };
 
-    return (
-        <UserContext.Provider
-            value={{
-                user,
-                login,
-                logout
-            }}
-        >
-            {children}
-        </UserContext.Provider>
-    );
+  const logout = async () => {
+    let response = await fetch(`${domain}/api/logout`, {
+      method: "POST",
+      credentials: "include",
+    });
+
+    if (response.status === 200) {
+      setUser({});
+    }
+  };
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        login,
+        logout,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
 };
 
 export const useUserContext = () => useContext(UserContext);
